fb211c5c353ab3038f6b0f666e31e4adbcc4e9a5
almerav <99607763+almerav@users.noreply.github.com>
2024-02-27 21:47:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,19 +11,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
-#Input the relevant libraries
-import numpy as np
-import pandas as pd
-import streamlit as st
-import altair as alt
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-
 # Define the Streamlit app
 def app():
 
@@ -103,8 +90,3 @@ def app():
 #run the app
 if __name__ == "__main__":
     app()
-
-#run the app
-if __name__ == "__main__":
-    app()
-
